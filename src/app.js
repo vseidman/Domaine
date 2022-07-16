980d2860e21c3d9fadfd7200f83a5dc436d5558c
vseidman <victorseidman@gmail.com>
2022-07-16 19:13:06 +0000

    Proyecto Listo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#domain").innerHTML = generateDomain();
+};
+let generateDomain = () => {
+  let person = ["La", "Mi"];
+  let subject = ["increible", "gran", "grandiosa"];
+  let action = ["pagina", "web", "gato", "perro"];
+  let object = [".com", ".org", ".net"];
+
+  let perInx = Math.floor(Math.random() * person.length);
+  let subInx = Math.floor(Math.random() * subject.length);
+  let acInx = Math.floor(Math.random() * action.length);
+  let obInx = Math.floor(Math.random() * object.length);
+
+  return (
+    person[perInx] +
+    "" +
+    subject[subInx] +
+    "" +
+    action[acInx] +
+    "" +
+    object[obInx]
+  );
 };
